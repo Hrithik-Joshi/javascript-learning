3543c77472c52d85d4ac57c4e273db4843957f24
Falconer951 <84170164+Hrithik-Joshi@users.noreply.github.com>
2025-01-31 02:09:54 +0530

    datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,47 @@
+/*JavaScript has 8 Datatypes
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object
+*/
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+let temp1 = "Volvo" + 16 + 4 //Volvo164
+
+let temp2 = 16 + 4 + "Volvo" //20Volvo
+
+/*Most programming languages have many number types:
+Whole numbers (integers):
+byte (8-bit), short (16-bit), int (32-bit), long (64-bit)
+Real numbers (floating-point):
+float (32-bit), double (64-bit).
+
+Javascript numbers are always one type:
+double (64-bit floating point).
+*/
+
+let temp3 = BigInt("123456789012345678901234567890");
