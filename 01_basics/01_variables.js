bd550b55724a9f00bd8fb4cbf32a7d0726a78113
Falconer951 <84170164+Hrithik-Joshi@users.noreply.github.com>
2025-01-30 23:12:53 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+// The let and const keywords were added to JavaScript in 2015.
+const user_id = 1234
+let user_name = "Hrithik Joshi"
+
+// The var keyword was used in all JavaScript code from 1995 to 2015.
+var user_password = "abc@123" //now we dont use var
+
+let user_age //undefined
+user_city = "Lucknow"
+typeof(user_city)
+
+console.table([user_id,user_name,user_password,user_age,user_city,typeof(user_city)])
