b695fb45053a14525d47f45de101b36519be43b0
Falconer951 <84170164+Hrithik-Joshi@users.noreply.github.com>
2025-02-10 23:08:46 +0530

    arrays in js

--- 01_basics/06_arrays.js
+++ 01_basics/06_arrays.js
@@ -13,5 +13,10 @@ console.log(temp_heores)
 
 // flattening of array
 const another_array = [1, 2, 3, [4, 5, 6], 7, 8, [9, 10, [11, 12]]];
-const fixed_array = another_array.flat(infinity); // define the depth upto how much you want to flat
+const fixed_array = another_array.flat(2); // define the depth upto how much you want to flat
 console.log(fixed_array);
+
+//Convert Array to string
+const my_array = ["Hrithik","Joshi"]
+let name_string = my_array.join(" ")
+console.log(name_string) // now we can perform the operations related to string
