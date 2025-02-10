31cbb9dd87c3da966198234b5dce3560ff769545
Falconer951 <84170164+Hrithik-Joshi@users.noreply.github.com>
2025-02-10 23:05:52 +0530

    arrays in js

---
+++ 01_basics/06_arrays.js
@@ -0,0 +1,17 @@
+const marvel_heroes = ["IronMan", "Thor", "Hulk"];
+const dc_heroes = ["Superman", "Flash", "Batman"];
+
+// marvel_heroes.push(dc_heroes)
+//console.log(marvel_heroes) //this is wrong way
+
+// const temp_heores = marvel_heroes.concat(dc_heroes)
+// console.log(temp_heores)
+
+// other way using spread operator which is used professionally
+const temp_heores = [...marvel_heroes, ...dc_heroes]
+console.log(temp_heores)
+
+// flattening of array
+const another_array = [1, 2, 3, [4, 5, 6], 7, 8, [9, 10, [11, 12]]];
+const fixed_array = another_array.flat(infinity); // define the depth upto how much you want to flat
+console.log(fixed_array);
